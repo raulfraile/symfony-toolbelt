309d9bc9caa154aa72958e4a23f05dd8c201b677
Javier Eguiluz <javier.eguiluz@gmail.com>
2014-08-26 07:50:26 +0200

    Added a "help" command which is displayed by default when executing the "symfony" command (this allows us to simplify the default help message of Windows, which is overwhelming for newcomers)

---
+++ src/Symfony/Toolbelt/HelpCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Symfony\Toolbelt;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelpCommand extends Command
+{
+    private $appVersion;
+
+    public function __construct($appVersion)
+    {
+        parent::__construct();
+
+        $this->appVersion = $appVersion;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('help')
+            ->setDescription('Symfony Toolbelt Help.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf("\n <info>Symfony Toolbelt</info> <comment>%s</comment>", $this->appVersion));
+        $output->writeln(" ~~~~~~~~~~~~~~~~~~~~");
+
+        $output->writeln(" This project provides several utilities for managing Symfony.\n");
+
+        $output->writeln(" Available commands:");
+
+        $output->writeln("   <info>new <dir-name></info>  Creates a new Symfony project in the given directory.");
+        $output->writeln("                   Example: <comment>$ symfony new blog/</comment>\n");
+    }
+}
